c48a1b346cd8423341814021f33c0d2a6079c8ba
ArtemGr <artemciy@gmail.com>
2015-12-06 12:22:16 +0300

    Duh, this Sublime Text git commit is very odd. Committing the missing changes now.

--- ip2location.rs
+++ ip2location.rs
@@ -1,9 +1,14 @@
+#![feature(libc)]
+#![allow(dead_code, non_camel_case_types, non_upper_case_globals)]
+
+#[macro_use] extern crate gstuff;
+extern crate libc;
+
 use std::ffi::CStr;
 use std::io::Write;
 use std::mem::uninitialized;
 use std::ptr::null_mut;
 use std::str::from_utf8_unchecked;
-use super::TwoBytes;
 
 pub type uint8_t = u8;
 pub type int32_t = i32;
@@ -15,16 +20,14 @@ include! ("/usr/local/include/ip2location.rs");
 pub struct Ip2Location (*mut IP2Location);
 impl Ip2Location {
   fn open (path: &str) -> Result<Ip2Location, String> {
-    let mut pathz: [u8; 256] = unsafe {uninitialized()}; let pathz = format! ("{}\0", path);
-    let i2l = unsafe {IP2Location_open (pathz.as_ptr() as *mut i8)};
+    let i2l = unsafe {IP2Location_open (format! ("{}\0", path) .as_ptr() as *mut i8)};
     if i2l == null_mut() {return ERR! ("!IP2Location_open ({})", path)}
     let rc = unsafe {IP2Location_open_mem (i2l, IP2LOCATION_SHARED_MEMORY)};
     if rc != 0 {return ERR! ("!IP2Location_open_mem")}
     Ok (Ip2Location (i2l))}
 
   /// Get a country from the IP.
-  pub fn ip2country (&self, ip: &str, debug: &Debug) -> Result<Option<TwoBytes>, String> {
-    let _ = debug.enter ("ip2country");
+  pub fn ip2country (&self, ip: &str) -> Result<Option<[u8; 2]>, String> {
     let mut ipz: [u8; 64] = unsafe {uninitialized()}; let ipz = gstring! (ipz, {try_s! (write! (ipz, "{}\0", ip))});
     assert! (self.0 != null_mut());
     let rec = unsafe {IP2Location_get_country_short (self.0, ipz.as_ptr() as *mut i8)};
@@ -33,8 +36,7 @@ impl Ip2Location {
     let country = unsafe {CStr::from_ptr ((*rec).country_short)} .to_bytes();
     if country == b"-" {return Ok (None)}
     if country.len() != 2 {return ERR! ("ip2country] !iso2: '{}'.", unsafe {from_utf8_unchecked (country)})}
-    let country = TwoBytes ([country[0], country[1]]);
-    debug! (debug, "ip2country ({}) = {}", ip, country);
+    let country = [country[0], country[1]];
     unsafe {IP2Location_free_record (rec)};
     Ok (Some (country))}}
 
